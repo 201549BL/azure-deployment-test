79d2cf3f83133e695d4cabd2e7b46cc295b75c1e
Eirik Bjørndal <eirik.bjorndal@gmail.com>
2025-02-04 10:19:16 +0100

    try again

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { connection } from "next/server";
 
-async function getServerSideNumber() {
-  "use server";
-  return Math.floor(Math.random() * 101);
-}
 
 export default async function Home() {
   await connection();
 
-  const number = getServerSideNumber();
+  const number = Math.random() * 100;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
